6e4f56d099f874495d742a67d68ea087a826f243
syukronrm <syukronrifai@gmail.com>
2016-08-23 15:46:24 +0700

    add

--- 09-mongodb/routes/index.js
+++ 09-mongodb/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
-var url = 'mongodb:://localhost:27017/test';
+var url = 'mongodb://127.0.0.1:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,6 +19,7 @@ router.get('/get-data', function(req, res, next) {
 		cursor.forEach(function(doc, err) {
 			assert.equal(null, err);
 			resultArray.push(doc);
+			console.log(doc);
 		}, function() {
 			db.close();
 			res.render('index', {items: resultArray});
@@ -29,24 +31,55 @@ router.post('/insert', function(req, res, next) {
 	var item = {
 		title : req.body.title,
 		content : req.body.content,
-		author : req.body.author,
-	}
+		author : req.body.author
+	};
 	mongo.connect(url, function(err, db) {
 		assert.equal(null, err);
-		db.collection('user-data').insertOne(item, function(err, db) {
+		db.collection('user-data').insertOne(item, function(err, result) {
 			assert.equal(null, err);
 			console.log('Item inserted.');
+			console.log(result);
 			db.close();
 		});
 	});
+	res.redirect('/');
 });
 
 router.post('/update', function(req, res, next) {
-	
+	var itemId = {
+		_id : objectId(req.body.id)
+	};
+	var item = {
+		title : req.body.title,
+		content : req.body.content,
+		author : req.body.author
+	};
+	mongo.connect(url, function(err, db) {
+		assert.equal(null, err);
+		db.collection('user-data').updateOne(itemId, {$set : item}, function(err, result) {
+			assert.equal(null, err);
+			console.log('Item updated');
+			console.log(itemId);
+			console.log(item);
+			db.close();
+		})
+	})
+	res.redirect('/');
 });
 
 router.post('/delete', function(req, res, next) {
-	
+	var itemId = {
+		_id : objectId(req.body.id)
+	};
+	mongo.connect(url, function(err, db) {
+		assert.equal(null, err);
+		db.collection('user-data').deleteOne(itemId, function(err, result) {
+			assert.equal(null, err);
+			console.log('Item deleted');
+			db.close();
+		})
+	})
+	res.redirect('/');
 });
 
 module.exports = router;
